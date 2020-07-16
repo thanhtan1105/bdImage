019ea08bf54c38d4fcc6ddd72cf95289b7763df4
tanle <thanhtan1105@gmail.com>
2020-07-17 00:27:41 +0700

    update logic again

--- library/bdImage/bdApi/ControllerApi/Post.php
+++ library/bdImage/bdApi/ControllerApi/Post.php
@@ -26,8 +26,7 @@ class bdImage_bdApi_ControllerApi_Post extends XFCP_bdImage_bdApi_ControllerApi_
                         $data[bdImage_Helper_Data::IMAGE_URL] === $threadImageData[bdImage_Helper_Data::IMAGE_URL] ? true : false,
                     'image_is_cover' =>
                         ($data[bdImage_Helper_Data::IMAGE_URL] === $threadImageData[bdImage_Helper_Data::IMAGE_URL]
-                            & (isset($threadImageData['is_cover'])
-                                ? (empty($threadImageData['is_cover']) ? false : $threadImageData['is_cover']) : false)) ? true : false
+                            & (!empty($threadImageData['is_cover']) ? true : false)) ? true : false
                 );
             }
             $response->params['post_images'] = $postBodyImages;
